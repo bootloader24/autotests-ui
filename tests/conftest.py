693e9a1f092c2487eccb67e602491edfbea54b72
bootloader24 <bootloader@student.su>
2026-05-15 12:18:08 +0500

    6.5 Фикстуры Pytest. Использование встроенных фикстур pytest-playwright.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,17 +1,16 @@
 import pytest  # Импортируем pytest
-from playwright.sync_api import sync_playwright, \
+from playwright.sync_api import Playwright, \
     Page  # Импортируем класс страницы, будем использовать его для аннотации типов
 
 
-@pytest.fixture  # Объявляем фикстуру, по умолчанию скоуп function, то что нам нужно
-def chromium_page() -> Page:  # Аннотируем возвращаемое фикстурой значение
+@pytest.fixture  # Используем фикстуру playwright
+def chromium_page(playwright: Playwright) -> Page:  # Аннотируем возвращаемое фикстурой значение
     # Ниже идет инициализация и открытие новой страницы
-    with sync_playwright() as playwright:
-        # Запускаем браузер
-        browser = playwright.chromium.launch(headless=False)
+    # Запускаем браузер
+    browser = playwright.chromium.launch(headless=False)
 
-        # Передаем страницу для использования в тесте
-        yield browser.new_page()
+    # Передаем страницу для использования в тесте
+    yield browser.new_page()
 
-        # Закрываем браузер после выполнения тестов
-        browser.close()
+    # Закрываем браузер после выполнения тестов
+    browser.close()
