05f71b8c0fdceb893e7f6624d1631eebf3d16060
bootloader24 <bootloader@student.su>
2026-05-17 15:34:59 +0500

    7.1 Знакомство с PageObject. Практика работы с Page Object.

---
+++ pages/dashboard_page.py
@@ -0,0 +1,16 @@
+from playwright.sync_api import Page, expect
+
+from pages.base_page import BasePage
+
+
+class DashboardPage(BasePage):
+    def __init__(self, page: Page):
+        super().__init__(page)
+
+        # Локаторы элементов страницы
+        self.dashboard_title = page.get_by_test_id('dashboard-toolbar-title-text')
+
+    # Метод для проверки видимости и текста заголовка “Dashboard”
+    def check_visible_dashboard_title(self):
+        expect(self.dashboard_title).to_be_visible()
+        expect(self.dashboard_title).to_have_text("Dashboard")
--- tests/test_registration.py
+++ tests/test_registration.py
@@ -1,33 +1,13 @@
-import pytest  # Импортируем библиотеку pytest
-from playwright.sync_api import expect, Page
+import pytest
+
+from pages.dashboard_page import DashboardPage
+from pages.registration_page import RegistrationPage
 
 
-# Запуск только этого теста: python -m pytest -m registration -s -v
-@pytest.mark.regression  # Добавили маркировку regression
-@pytest.mark.registration  # Добавили маркировку registration
-def test_successful_registration(chromium_page: Page):
-    # Переходим на страницу регистрации
-    chromium_page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
-
-    # Находим поле "Email" и заполняем его
-    email_input = chromium_page.get_by_test_id('registration-form-email-input').locator('input')
-    email_input.fill("user.name@gmail.com")
-
-    # Находим поле "Username" и заполняем его
-    username_input = chromium_page.get_by_test_id('registration-form-username-input').locator('input')
-    username_input.fill("username")
-
-    # Находим поле "Password" и заполняем его
-    password_input = chromium_page.get_by_test_id('registration-form-password-input').locator('input')
-    password_input.fill("password")
-
-    # Находим кнопку "Registration" и кликаем на неё
-    registration_button = chromium_page.get_by_test_id('registration-page-registration-button')
-    registration_button.click()
-
-    # Проверяем, что произошёл редирект на страницу "Dashboard"
-    expect(chromium_page).to_have_url('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/dashboard')
-
-    # Проверяем заголовок страницы "Dashboard"
-    dashboard_title = chromium_page.get_by_test_id('dashboard-toolbar-title-text')
-    expect(dashboard_title).to_be_visible()
+@pytest.mark.regression
+@pytest.mark.registration
+def test_successful_registration(registration_page: RegistrationPage, dashboard_page: DashboardPage):
+    registration_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
+    registration_page.fill_registration_form("user.name@gmail.com", "username", "password")
+    registration_page.click_registration_button()
+    dashboard_page.check_visible_dashboard_title()
