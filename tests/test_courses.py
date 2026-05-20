5acc1a21e6e2212641e4693bdf9ad7eec9b16dd1
bootloader24 <bootloader@student.su>
2026-05-20 20:14:51 +0500

    7.4 Практикуемся в реализации PageComponent. Реализация и использование компонента ImageUploadWidgetComponent.

--- tests/test_courses.py
+++ tests/test_courses.py
@@ -31,11 +31,9 @@ def test_create_course(courses_list_page: CoursesListPage, create_course_page: C
     create_course_page.check_visible_create_course_title()
     # 3. Проверить, что кнопка создания курса недоступна для нажатия
     create_course_page.check_disabled_create_course_button()
-    # 4. Убедиться, что отображается пустой блок для предпросмотра изображения
-    create_course_page.check_visible_image_preview_empty_view()
-    # 5. Проверить, что блок загрузки изображения отображается в состоянии, когда картинка не выбрана
-    create_course_page.check_visible_image_upload_view(is_image_uploaded=False)
-    # 6. Проверить, что форма создания курса отображается и содержит значения по умолчанию.
+    # 4. Убедиться, что отображается пустой блок для предпросмотра и загрузки изображения
+    create_course_page.image_upload_widget.check_visible(is_image_uploaded=False)
+    # 5. Проверить, что форма создания курса отображается и содержит значения по умолчанию.
     create_course_page.check_visible_create_course_form(
         title="",
         estimated_time="",
@@ -43,18 +41,18 @@ def test_create_course(courses_list_page: CoursesListPage, create_course_page: C
         max_score="0",
         min_score="0",
     )
-    # 7. Проверить наличие заголовка "Exercises"
+    # 6. Проверить наличие заголовка "Exercises"
     create_course_page.check_visible_exercises_title()
-    # 8. Проверить наличие кнопки создания задания
+    # 7. Проверить наличие кнопки создания задания
     create_course_page.check_visible_create_exercise_button()
-    # 9. Убедиться, что отображается блок с пустыми заданиями
+    # 8. Убедиться, что отображается блок с пустыми заданиями
     create_course_page.check_visible_exercises_empty_view()
 
-    # 10. Загрузить изображение для превью курса
-    create_course_page.upload_preview_image("./testdata/files/image.png")
-    # 11. Убедиться, что блок загрузки изображения отображает состояние, когда картинка успешно загружена
-    create_course_page.check_visible_image_upload_view(is_image_uploaded=True)
-    # 12. Заполнить форму создания курса значениями
+    # 9. Загрузить изображение для превью курса
+    create_course_page.image_upload_widget.upload_preview_image("./testdata/files/image.png")
+    # 10. Убедиться, что блок загрузки изображения отображает состояние, когда картинка успешно загружена
+    create_course_page.image_upload_widget.check_visible(is_image_uploaded=True)
+    # 11. Заполнить форму создания курса значениями
     create_course_page.fill_create_course_form(
         title="Playwright",
         estimated_time="2 weeks",
@@ -62,12 +60,12 @@ def test_create_course(courses_list_page: CoursesListPage, create_course_page: C
         max_score="100",
         min_score="10",
     )
-    # 13. Нажать на кнопку создания курса
+    # 12. Нажать на кнопку создания курса
     create_course_page.click_create_course_button()
 
-    # 14. После редиректа на страницу со списком курсов проверить наличие компонента заголовка "Courses"
+    # 13. После редиректа на страницу со списком курсов проверить наличие компонента заголовка "Courses"
     courses_list_page.toolbar_view.check_visible()
-    # 15. Проверить корректность отображаемых данных на карточке курса
+    # 14. Проверить корректность отображаемых данных на карточке курса
     courses_list_page.course_view.check_visible(
         index=0,
         title="Playwright",
