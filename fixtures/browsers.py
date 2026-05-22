f8831b61a92031090dbc456e05ae2063d73f2a80
bootloader24 <bootloader@student.su>
2026-05-22 23:55:45 +0500

    7.7 Наращивание тестовой базы и рефакторинг. Рефакторинг фикстуры initialize_browser_state.

--- fixtures/browsers.py
+++ fixtures/browsers.py
@@ -2,6 +2,8 @@ import pytest  # Импортируем pytest
 from playwright.sync_api import Playwright, \
     Page  # Импортируем класс страницы, будем использовать его для аннотации типов
 
+from pages.authentication.registration_page import RegistrationPage
+
 
 @pytest.fixture  # Используем фикстуру playwright
 def chromium_page(playwright: Playwright) -> Page:  # Аннотируем возвращаемое фикстурой значение
@@ -19,20 +21,16 @@ def initialize_browser_state(playwright: Playwright):
     browser = playwright.chromium.launch(headless=True)
     context = browser.new_context()  # Создание контекста
     page = context.new_page()  # Создание страницы
+
+    # Работаем с регистрационной страницей через Page Object
+    registration_page = RegistrationPage(page=page)
     # Переходим на страницу регистрации
-    page.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
-    # Заполняем поле "Email"
-    email_input = page.get_by_test_id('registration-form-email-input').locator('input')
-    email_input.fill("user.name@gmail.com")
-    # Заполняем поле "Username"
-    username_input = page.get_by_test_id('registration-form-username-input').locator('input')
-    username_input.fill("username")
-    # Заполняем поле "Password"
-    password_input = page.get_by_test_id('registration-form-password-input').locator('input')
-    password_input.fill("password")
+    registration_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/registration")
+    # Заполняем форму регистрации
+    registration_page.registration_form.fill(email='user.name@gmail.com', username='username', password='password')
     # Кликаем на кнопку "Registration"
-    registration_button = page.get_by_test_id('registration-page-registration-button')
-    registration_button.click()
+    registration_page.click_registration_button()
+
     # Сохраняем состояние браузера (куки и localStorage) в файл для дальнейшего использования
     context.storage_state(path="browser-state.json")
     # Закрываем браузер
