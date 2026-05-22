fc8df652e0af2d5c417b7dbf13acd0295887962c
bootloader24 <bootloader@student.su>
2026-05-23 01:12:40 +0500

    7.7 Наращивание тестовой базы и рефакторинг. Практикуемся в написании автотестов с использованием Page Object, Page Component, Page Factory.

--- tests/courses/test_courses.py
+++ tests/courses/test_courses.py
@@ -67,3 +67,49 @@ class TestCourses:
             min_score="10",
             estimated_time="2 weeks"
         )
+
+    def test_edit_course(self, courses_list_page: CoursesListPage, create_course_page: CreateCoursePage):
+        # Открыть страницу создания курса
+        create_course_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses/create")
+
+        # Заполнить форму создания курса валидными данными, загрузить изображение и нажать кнопку создания курса.
+        create_course_page.create_course_form.fill(
+            title="Playwright",
+            estimated_time="2 weeks",
+            description="Playwright",
+            max_score="100",
+            min_score="10",
+        )
+        create_course_page.image_upload_widget.upload_preview_image("./testdata/files/image.png")
+        create_course_page.create_course_toolbar_view.click_create_course_button()
+
+        # Проверить, что на странице с курсами отображается карточка ранее созданного курса.
+        courses_list_page.course_view.check_visible(
+            index=0,
+            title="Playwright",
+            max_score="100",
+            min_score="10",
+            estimated_time="2 weeks"
+        )
+
+        # Через меню карточки курса нажать на кнопку "Edit".
+        courses_list_page.course_view.menu.click_edit(index=0)
+
+        # Изменить поля: title, estimated time, description, max score, min score, нажать на кнопку сохранения изменений
+        create_course_page.create_course_form.fill(
+            title="Selenium",
+            estimated_time="3 weeks",
+            description="Selenium",
+            max_score="120",
+            min_score="20",
+        )
+        create_course_page.create_course_toolbar_view.click_create_course_button()
+
+        # Проверить, что на странице с курсами отображается карточка курса с обновленными данными.
+        courses_list_page.course_view.check_visible(
+            index=0,
+            title="Selenium",
+            max_score="120",
+            min_score="20",
+            estimated_time="3 weeks"
+        )
