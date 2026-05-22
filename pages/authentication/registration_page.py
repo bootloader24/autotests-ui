5530d834ac0f84c045cfd937434afbee819a57ef
bootloader24 <bootloader@student.su>
2026-05-23 00:51:39 +0500

    7.7 Наращивание тестовой базы и рефакторинг. Добавление автотеста test_navigate_from_authorization_to_registration.

--- pages/authentication/registration_page.py
+++ pages/authentication/registration_page.py
@@ -1,3 +1,5 @@
+import re
+
 from playwright.sync_api import Page
 
 from components.authentication.registration_form_component import RegistrationFormComponent
@@ -26,3 +28,4 @@ class RegistrationPage(BasePage):
     # Метод для нажатия на ссылку "Login"
     def click_login_link(self):
         self.login_link.click()
+        self.check_current_url(re.compile(".*/#/auth/login"))
--- tests/authentication/test_authorization.py
+++ tests/authentication/test_authorization.py
@@ -51,3 +51,13 @@ class TestAuthorization:
         login_page.login_form.fill(email=email, password=password)  # Заполняем форму авторизации
         login_page.click_login_button()  # Нажимаем кнопку "Login"
         login_page.check_visible_wrong_email_or_password_alert()  # Проверяем наличие сообщения об ошибке
+
+    def test_navigate_from_authorization_to_registration(
+            self,
+            login_page: LoginPage,
+            registration_page: RegistrationPage
+    ):
+        login_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/auth/login")
+        login_page.click_registration_link()
+
+        registration_page.registration_form.check_visible(email="", username="", password="")
