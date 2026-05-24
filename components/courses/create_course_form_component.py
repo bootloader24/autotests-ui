46816e8b56956547aba98240d46769bb9be237d8
bootloader24 <bootloader@student.su>
2026-05-24 16:36:22 +0500

    8.3 Добавление Allure шагов в автотесты. Практика работы с allure.step.

--- components/courses/create_course_form_component.py
+++ components/courses/create_course_form_component.py
@@ -1,3 +1,4 @@
+import allure
 from playwright.sync_api import Page
 
 from components.base_component import BaseComponent
@@ -19,6 +20,7 @@ class CreateCourseFormComponent(BaseComponent):
         self.max_score_input = Input(page, 'create-course-form-max-score-input', name='Max score input')
         self.min_score_input = Input(page, 'create-course-form-min-score-input', name='Min score input')
 
+    @allure.step('Check visible create course form "{title}"')
     def check_visible(
             self,
             title: str,
@@ -42,6 +44,7 @@ class CreateCourseFormComponent(BaseComponent):
         self.min_score_input.check_visible()
         self.min_score_input.check_have_value(min_score)
 
+    @allure.step('Fill create course form')
     def fill(
             self,
             title: str,
