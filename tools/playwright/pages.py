16651871703de8d6ce654a1f5a4b58ee062b91f6
bootloader24 <bootloader@student.su>
2026-05-26 15:38:05 +0500

    9.2 Запуск автотестов на разных браузерах. Запускаем автотесты на разных браузерах.

--- tools/playwright/pages.py
+++ tools/playwright/pages.py
@@ -1,7 +1,7 @@
 import allure
 from playwright.sync_api import Playwright, Page
 
-from config import settings # импорт настроек
+from config import settings, Browser  # импорт настроек
 
 """
 Параметры context.tracing.start():
@@ -13,9 +13,10 @@ from config import settings # импорт настроек
 def initialize_playwright_page(
         playwright: Playwright,
         test_name: str,
+        browser_type: Browser,
         storage_state: str | None = None
 ) -> Page:
-    browser = playwright.chromium.launch(headless=settings.headless)
+    browser = playwright[browser_type].launch(headless=settings.headless)
     # Создаем контекст для новой сессии браузера с указанием директории для сохранения видеозаписей
     context = browser.new_context(
         base_url=settings.get_base_url(),
